7511ede316a86661aa156f1a8106f02dc3d5ef47
hakbar0 <haseeb2000@hotmail.com>
2023-03-11 18:49:49 +0000

    able to rotate the robot

--- firstwalk.py
+++ firstwalk.py
@@ -22,13 +22,40 @@ def publisher():
         pub.publish(desired_velocity)
         rate.sleep()
 
-    # Set the desired velocity to move backwards
-    desired_velocity.linear.x = -0.2 # Backward with 0.2 m/sec.
+    # Stop the robot for 1 second
+    desired_velocity.linear.x = 0.0
+    desired_velocity.angular.z = 0.0
+    for i in range(10):
+        pub.publish(desired_velocity)
+        rate.sleep()
 
-    # Move backwards for 3 seconds (30 iterations at 10Hz)
+    # Set the desired angular velocity to rotate the robot
+    desired_velocity.angular.z = 0.5 # Rotate at 0.5 rad/sec (clockwise)
+
+    # Rotate the robot for 3 seconds (30 iterations at 10Hz)
     for i in range(30):
         pub.publish(desired_velocity)
         rate.sleep()
+
+    # Stop the robot for 1 second
+    desired_velocity.linear.x = 0.0
+    desired_velocity.angular.z = 0.0
+    for i in range(10):
+        pub.publish(desired_velocity)
+        rate.sleep()
+
+    # Set the desired angular velocity to rotate the robot in the opposite direction
+    desired_velocity.angular.z = -0.5 # Rotate at -0.5 rad/sec (counterclockwise)
+
+    # Rotate the robot for 3 seconds (30 iterations at 10Hz)
+    for i in range(30):
+        pub.publish(desired_velocity)
+        rate.sleep()
+
+    # Stop the robot
+    desired_velocity.linear.x = 0.0
+    desired_velocity.angular.z = 0.0
+    pub.publish(desired_velocity)
 
 if __name__ == "__main__":
     try:
